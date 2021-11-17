e4a16d62ffa5a0e2c037f35e70baf0b78bd1dfda
Nathan Nolan <nnolan@setsail.co>
2021-11-17 14:45:40 -0600

    tfvars?

--- managed-stack/main.tf
+++ managed-stack/main.tf
@@ -4,15 +4,27 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "spacelift_current_stack" "this" {}
+
+resource "spacelift_environment_variable" "tf-vars" {
+  stack_id   = spacelift_stack.this.id
+  name       = "TF_CLI_ARGS"
+  value      = "-var-file=test.tfvars"
+  write_only = false
+}
+
 resource "random_password" "secret" {
   length  = 32
   special = true
 }
+
+
 
 module "ec2-module" {
   source  = "spacelift.io/setsailco/ec2-module/default"
   version = "0.0.1"
 
   # Required inputs 
-  name = "test_spacelift"# string
+  name = var.name # string
 }
+
