7ee9d036cf1dc0385399f958052cf4072cc7a3e9
Nathan Nolan <nnolan@setsail.co>
2021-11-17 14:14:32 -0600

    updating managed stack to pull ec2

--- managed-stack/main.tf
+++ managed-stack/main.tf
@@ -1,6 +1,16 @@
 # This resource here is to show you how plan policies work.
 
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "random_password" "secret" {
   length  = 32
   special = true
+}
+
+module "ec2" {
+  source = "setsailco.app.spacelift.io/module/ec2-module"
+  version = "0.0.1"
+  name = "test_spacelift"
 }
