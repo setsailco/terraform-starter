caffae24bae2d753caaaca0cffac888d44b63736
Nathan Nolan <nnolan@setsail.co>
2021-11-17 15:33:53 -0600

    Removing count

--- managed-stack/main.tf
+++ managed-stack/main.tf
@@ -1,8 +1,8 @@
 # This resource here is to show you how plan policies work.
 
-provider "aws" {
-  region = "us-east-1"
-}
+#provider "aws" {
+#  region = "us-east-1"
+#}
 
 resource "random_password" "secret" {
   length  = 32
