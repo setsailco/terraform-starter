24aecb8662ef1ee99061a1ddfa3ca6449b7ce1db
Nathan Nolan <nnolan@setsail.co>
2022-01-05 09:24:49 -0600

    ec2 module being weird

--- modules.tf
+++ modules.tf
@@ -3,6 +3,6 @@ resource "spacelift_module" "ec2-module" {
   name              = "ec2-module"
   branch            = "main"
   description       = "Ec2 terraform module"
-  project_root      = "terraform_modules/ec2-module"
+  project_root      = "ec2-module"
   repository        = "terraform-starter"
 }
