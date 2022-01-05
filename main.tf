997facce993b3885d1d2252ac9bda15cc1af62b3
Nathan Nolan <nnolan@setsail.co>
2022-01-05 08:43:44 -0600

    Adding provider info

--- main.tf
+++ main.tf
@@ -2,10 +2,16 @@ terraform {
   required_providers {
     spacelift = {
       source = "spacelift-io/spacelift"
+      version = "0.1.8"
     }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.64.2"
+    }
+
+    local = {
+      source = "hashicorp/local"
+      version = "2.1.0"
     }
   }
 }
